75e353224bfaf660b218eb51443efcd7541b8707
DURGASI MAHESH KUMAR <124244425+Mahesh-100@users.noreply.github.com>
2024-07-09 07:56:19 +0530

    updated

---
+++ user-management/src/main/java/com/amzur/user_management/service/UserService.java
@@ -0,0 +1,23 @@
+package com.amzur.user_management.service;
+
+import java.util.List;
+import com.amzur.user_management.dto.request.OrderResponse;
+import com.amzur.user_management.dto.request.UserRequest;
+import com.amzur.user_management.dto.response.UserResponse;
+
+
+public interface UserService {
+	
+	public UserResponse save(UserRequest userRequest);
+	
+	 public List<UserResponse> findAll();
+	 
+	public UserResponse findByEmail(String email,String password);
+
+	public void deleteById(long userId);
+	
+	public UserResponse findById(Long id);
+
+	public List<OrderResponse> getUserOrders(String email, String password);
+
+}
