552b47115dbcd391818c2a7bdbf6325dab7bd06d
DURGASI MAHESH KUMAR <124244425+Mahesh-100@users.noreply.github.com>
2024-07-13 11:57:33 +0530

    updated

--- src/main/java/com/amzur/user_management/dto/response/OrderResponse.java
+++ src/main/java/com/amzur/user_management/dto/response/OrderResponse.java
@@ -5,7 +5,11 @@ import lombok.Data;
 @Data
 public class OrderResponse {
   
+<<<<<<< Updated upstream
     private Long orderId;  
     private int bookId;
+=======
+    private Long orderId;   
+>>>>>>> Stashed changes
 }
 
